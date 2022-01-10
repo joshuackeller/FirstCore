74d60bd851c0cc19eb9c28f7fa02a9852175e078
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2022-01-10 10:23:35 -0700

    changes

---
+++ FirstCore/wwwroot/js/submitButton.js
@@ -0,0 +1,16 @@
+﻿//document.getElementById('submitButton').addEventListener("click",
+//    (e) => {
+//        e.preventDefault();
+//        alert("From: " + document.getElementById("textFrom").value +
+//            " Subject: " + document.getElementById("textSubject").value +
+//            " Message: " + document.getElementById("textMessage").value);
+//    });
+
+$('#submitButton').click(
+    (e) => {
+        (e).preventDefault();
+        alert("From: " + $("#textFrom").val() +
+            " Subject: " + $("#textSubject").val() +
+            " Message: " + $("#textMessage").val());
+        $("#picAnderson").fadeToggle('slow');
+    });
