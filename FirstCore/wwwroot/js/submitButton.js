a02a97fca3423412da5ed8a46aa996c7661773eb
joshuackeller <68955051+joshuackeller@users.noreply.github.com>
2022-01-12 09:48:29 -0700

    changes for rubric

--- FirstCore/wwwroot/js/submitButton.js
+++ FirstCore/wwwroot/js/submitButton.js
@@ -1,17 +1,53 @@
-﻿//document.getElementById('submitButton').addEventListener("click",
-//    (e) => {
-//        e.preventDefault();
-//        alert("From: " + document.getElementById("textFrom").value +
-//            " Subject: " + document.getElementById("textSubject").value +
-//            " Message: " + document.getElementById("textMessage").value);
-//    });
-
+﻿
 $('#submitButton').click(
     (e) => {
-        (e).preventDefault();
-        alert("From: " + $("#textFrom").val() +
-            " Subject: " + $("#textSubject").val() +
-            " Message: " + $("#textMessage").val());
-        $("#picAnderson").fadeToggle('slow');
-        alert('hello')
+        // Keep browser from refreshing
+        e.preventDefault();
+        // Calculate Grade
+        const grade = ($("#assignments").val() * .55) +
+            ($("#groupProject").val() * .05) +
+            ($("#quizzes").val() * .1) +
+            ($("#exams").val() * .2) +
+            ($("#intex").val() * .1);
+
+        // Calculate Letter Grade
+        var letterGrade = 'none'
+        if (grade >= 94) {
+            letterGrade = 'A'
+        } else if (grade >= 90 ) {
+            letterGrade = 'A-'
+        }
+        else if (grade >= 87) {
+            letterGrade = 'B+'
+        }
+        else if (grade >= 84) {
+            letterGrade = 'B'
+        }
+        else if (grade >= 80) {
+            letterGrade = 'B-'
+        }
+        else if (grade >= 77) {
+            letterGrade = 'C+'
+        }
+        else if (grade >= 74) {
+            letterGrade = 'C'
+        }
+        else if (grade >= 70) {
+            letterGrade = 'C-'
+        }
+        else if (grade >= 67) {
+            letterGrade = 'D+'
+        }
+        else if (grade >= 64) {
+            letterGrade = 'D'
+        }
+        else if (grade >= 60) {
+            letterGrade = 'D-'
+        }
+        else{
+            letterGrade = 'E'
+        }
+        // Send alert to display final percent and letter grade
+        alert('Final Percent: ' + grade.toFixed(2)
+              + '\n Letter Grade: ' + letterGrade);
     });
